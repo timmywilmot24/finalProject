fe1091a17743f16f4ddeeffd7a857183cfa701b9
timmywilmot24 <56287362+timmywilmot24@users.noreply.github.com>
2019-10-07 23:51:34 -0400

    Create data.js

---
+++ data.js
@@ -0,0 +1,5 @@
+import * as myObjects from './objects.js';
+
+let SamHowell = new myObjects.NCAAPlayer("Sam", "Howell", "pic here", "stat object", true, "UNC", "Timmy object", "QB");
+
+console.log(SamHowell.first);
