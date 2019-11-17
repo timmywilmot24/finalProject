7f52bec7107cbb9ceae5ab1abed68f7658e32742
timmywilmot24 <56287362+timmywilmot24@users.noreply.github.com>
2019-11-16 23:24:02 -0500

    Update data.js

--- data.js
+++ data.js
@@ -1,86 +1,63 @@
-import * as myObjects from './objects.js';
 
-//NCAA players QB
+let fullTeams = ["Clemson", "Syracuse", "North Carolina State", "Duke", "Virginia", "Boston College", "Wake Forest", "Georgia Tech", "Miami", "Pittsburgh", "Virginia Tech", "Florida State", "North Carolina", "Louisville"];
+let teamAbb = ["CLE", "SYR", "NCST", "DUK", "UVA", "BC", "WF", "GT", "MFL", "PIT", "VT", "FSU", "NC", "LOU"];
 
-let QB1 = new myObjects.NCAAPlayer("Sam", "Howell", "pic here", "stat object", true, "North Carolina", "Timmy object", "QB");
-let QB2 = new myObjects.NCAAPlayer("Tobias", "Oliver", "pic here", "stat object", true, "Georgia Tech", "Timmy object", "QB");
-let QB3 = new myObjects.NCAAPlayer("Jawon", "Pass", "pic here", "stat object", true, "Louisville", "Timmy object", "QB");
-let QB4 = new myObjects.NCAAPlayer("Matt", "McKay", "pic here", "stat object", true, "NC State", "Timmy object", "QB");
-let QB5 = new myObjects.NCAAPlayer("Quentin", "Harris", "pic here", "stat object", true, "Duke", "Timmy object", "QB");
-let QB6 = new myObjects.NCAAPlayer("Kenney", "Pickett", "pic here", "stat object", true, "Pitt", "Timmy object", "QB");
-let QB7 = new myObjects.NCAAPlayer("N'Kosi", "Perry", "pic here", "stat object", true, "Miami", "Timmy object", "QB");
-let QB8 = new myObjects.NCAAPlayer("Anthony", "Brown", "pic here", "stat object", true, "Boston College", "Timmy object", "QB");
-let QB9 = new myObjects.NCAAPlayer("Ryan", "Willis", "pic here", "stat object", true, "Virginia Tech", "Timmy object", "QB");
-let QB10 = new myObjects.NCAAPlayer("Jamie", "Newman", "pic here", "stat object", true, "Wake Forest", "Timmy object", "QB");
-let QB11 = new myObjects.NCAAPlayer("James", "Blackman", "pic here", "stat object", true, "Florida State", "Timmy object", "QB");
-let QB12 = new myObjects.NCAAPlayer("Bryce", "Perkins", "pic here", "stat object", true, "Virginia", "Timmy object", "QB");
-let QB13 = new myObjects.NCAAPlayer("Trevor", "Lawrence", "pic here", "stat object", true, "Clemson", "Timmy object", "QB");
+import axios from 'axios';
 
-//NCAA players RB
+const pubRoot = new axios.create({
+    baseURL: "http://localhost:3000/public"
+});
 
-let RB1 = new myObjects.NCAAPlayer("Michael", "Carter", "pic here", "stat object", true, "North Carolina", "Timmy object", "RB");
-let RB2 = new myObjects.NCAAPlayer("Jordan", "Mason", "pic here", "stat object", true, "Georgia Tech", "Timmy object", "RB");
-let RB3 = new myObjects.NCAAPlayer("Antonio", "William", "pic here", "stat object", true, "North Carolina", "Timmy object", "RB");
-let RB4 = new myObjects.NCAAPlayer("Deon", "Jackson", "pic here", "stat object", true, "Duke", "Timmy object", "RB");
-let RB5 = new myObjects.NCAAPlayer("Brittain", "Brown", "pic here", "stat object", true, "Duke", "Timmy object", "RB");
-let RB6 = new myObjects.NCAAPlayer("Kenney", "Pickett", "pic here", "stat object", true, "Pitt", "Timmy object", "RB");
-let RB7 = new myObjects.NCAAPlayer("Deejay", "Dallas", "pic here", "stat object", true, "Miami", "Timmy object", "RB");
-let RB8 = new myObjects.NCAAPlayer("Moe", "Neal", "pic here", "stat object", true, "Syracuse", "Timmy object", "RB");
-let RB9 = new myObjects.NCAAPlayer("Ryan", "Willis", "pic here", "stat object", true, "Virginia Tech", "Timmy object", "RB");
-let RB10 = new myObjects.NCAAPlayer("Cade", "Carney", "pic here", "stat object", true, "Wake Forest", "Timmy object", "RB");
-let RB11 = new myObjects.NCAAPlayer("Cam", "Akers", "pic here", "stat object", true, "Florida State", "Timmy object", "RB");
-let RB12 = new myObjects.NCAAPlayer("Ricky", "Person", "pic here", "stat object", true, "NC State", "Timmy object", "RB");
-let RB13 = new myObjects.NCAAPlayer("Travis", "Etienne", "pic here", "stat object", true, "Clemson", "Timmy object", "RB");
+pubRoot.delete(`/teams`);
 
-//NCAA player WR
+function createTeam(teamCity, teamName, schedule, pic) {
+    pubRoot.post(`/teams/`+teamCity, {
+        data: {teamCity, teamName, schedule, pic}
+    })
+}
 
-let WR1 = new myObjects.NCAAPlayer("Lebron", "James", "pic here", "stat object", true, "North Carolina", "Timmy object", "WR");
-let WR2 = new myObjects.NCAAPlayer("Michael", "Jordan", "pic here", "stat object", true, "Georgia Tech", "Timmy object", "WR");
-let WR3 = new myObjects.NCAAPlayer("Collin", "Herd", "pic here", "stat object", true, "North Carolina", "Timmy object", "WR");
-let WR4 = new myObjects.NCAAPlayer("Michael", "Thomas", "pic here", "stat object", true, "Duke", "Timmy object", "WR");
-let WR5 = new myObjects.NCAAPlayer("Tiger", "Wood", "pic here", "stat object", true, "Duke", "Timmy object", "WR");
-let WR6 = new myObjects.NCAAPlayer("Anthony", "Davis", "pic here", "stat object", true, "Pitt", "Timmy object", "WR");
-let WR7 = new myObjects.NCAAPlayer("Antonio", "Brown", "pic here", "stat object", true, "Miami", "Timmy object", "WR");
-let WR8 = new myObjects.NCAAPlayer("AJ", "Green", "pic here", "stat object", true, "Syracuse", "Timmy object", "WR");
-let WR9 = new myObjects.NCAAPlayer("Devante", "Adams", "pic here", "stat object", true, "Virginia Tech", "Timmy object", "WR");
-let WR10 = new myObjects.NCAAPlayer("Hollywood", "Brown", "pic here", "stat object", true, "Wake Forest", "Timmy object", "WR");
-let WR11 = new myObjects.NCAAPlayer("Tyreke", "Hills", "pic here", "stat object", true, "Florida State", "Timmy object", "WR");
-let WR12 = new myObjects.NCAAPlayer("Jarvis", "Landry", "pic here", "stat object", true, "NC State", "Timmy object", "WR");
-let WR13 = new myObjects.NCAAPlayer("Jerry", "Rice", "pic here", "stat object", true, "Clemson", "Timmy object", "WR");
+let getTeams = async function() {
+    await axios({
+        method: "get",
+        url: 'http://api.sportradar.us/ncaafb-t1/teams/FBS/2018/REG/standings.json?api_key=sg8pf7bdjt5u8ueedttyytwx',
+    }).then(data => {
+        let teams = data.data.division.conferences[0].teams;
+        for (let i = 0; i < teams.length; i++) {
+            let market = teams[i].market;
+            if (teams[i].market == "Miami (FL)") {
+                market = "Miami";
+            }
+            createTeam(market, teams[i].name, [], 0);
+        }
+    });
+}
+getTeams();
 
-//NCAA player TE
-
-let TE1 = new myObjects.NCAAPlayer("Jimmy", "Graham", "pic here", "stat object", true, "North Carolina", "Timmy object", "TE");
-let TE2 = new myObjects.NCAAPlayer("George", "Kittle", "pic here", "stat object", true, "Georgia Tech", "Timmy object", "TE");
-let TE3 = new myObjects.NCAAPlayer("George", "Washington", "pic here", "stat object", true, "North Carolina", "Timmy object", "TE");
-let TE4 = new myObjects.NCAAPlayer("Kobe", "Bryant", "pic here", "stat object", true, "Duke", "Timmy object", "TE");
-let TE5 = new myObjects.NCAAPlayer("Charles", "Barkley", "pic here", "stat object", true, "Duke", "Timmy object", "TE");
-let TE6 = new myObjects.NCAAPlayer("Steph", "Curry", "pic here", "stat object", true, "Pitt", "Timmy object", "TE");
-let TE7 = new myObjects.NCAAPlayer("Skip", "Bayless", "pic here", "stat object", true, "Miami", "Timmy object", "TE");
-let TE8 = new myObjects.NCAAPlayer("King", "Henry", "pic here", "stat object", true, "Syracuse", "Timmy object", "TE");
-let TE9 = new myObjects.NCAAPlayer("Lauren", "Ammons", "pic here", "stat object", true, "Virginia Tech", "Timmy object", "TE");
-let TE10 = new myObjects.NCAAPlayer("Tommy", "OHern", "pic here", "stat object", true, "Wake Forest", "Timmy object", "TE");
-let TE11 = new myObjects.NCAAPlayer("Kenney", "Smith", "pic here", "stat object", true, "Florida State", "Timmy object", "TE");
-let TE12 = new myObjects.NCAAPlayer("Dwayne", "Wade", "pic here", "stat object", true, "NC State", "Timmy object", "TE");
-let TE13 = new myObjects.NCAAPlayer("Las", "Vegas", "pic here", "stat object", true, "Clemson", "Timmy object", "TE");
-
-
-//NCAA player DEF
-
-let DEF1 = new myObjects.NCAAPlayer("Defense", "North Carolina", "pic here", "stat object", true, "North Carolina", "Timmy object", "DEF");
-let DEF2 = new myObjects.NCAAPlayer("Defense", "NC State", "pic here", "stat object", true, "NC State", "Timmy object", "DEF");
-let DEF3 = new myObjects.NCAAPlayer("Defense", "Duke", "pic here", "stat object", true, "Duke", "Timmy object", "DEF");
-let DEF4 = new myObjects.NCAAPlayer("Defense", "Georgia Tech", "pic here", "stat object", true, "Georgia Tech", "Timmy object", "DEF");
-let DEF5 = new myObjects.NCAAPlayer("Defense", "Miami", "pic here", "stat object", true, "Miami", "Timmy object", "DEF");
-let DEF6 = new myObjects.NCAAPlayer("Defense", "Pittsburgh", "pic here", "stat object", true, "Pitt", "Timmy object", "DEF");
-let DEF7 = new myObjects.NCAAPlayer("Defense", "UVA", "pic here", "stat object", true, "UVA", "Timmy object", "DEF");
-let DEF8 = new myObjects.NCAAPlayer("Defense", "Virginia Tech", "pic here", "stat object", true, "Virginia Tech", "Timmy object", "DEF");
-let DEF9 = new myObjects.NCAAPlayer("Defense", "Boston College", "pic here", "stat object", true, "Boston College", "Timmy object", "DEF");
-let DEF10 = new myObjects.NCAAPlayer("Defense", "FSU", "pic here", "stat object", true, "FSU", "Timmy object", "DEf");
-let DEF11 = new myObjects.NCAAPlayer("Defense", "Louisville", "pic here", "stat object", true, "Louisville", "Timmy object", "DEf");
-let DEF12 = new myObjects.NCAAPlayer("Defense", "Notre Dame", "pic here", "stat object", true, "Notre Dame", "Timmy object", "DEF");
-let DEF13 = new myObjects.NCAAPlayer("Defense", "Syracuse", "pic here", "stat object", true, "Syracuse", "Timmy object", "DEF");
-let DEF13 = new myObjects.NCAAPlayer("Defense", "Wake Forest", "pic here", "stat object", true, "Wake Forest", "Timmy object", "DEF");
-
-//League Player
-let leaguePlayer1 = new myObjects.LeaguePlayer("Pandas", "David", "picture", "stats", "schedule", )
+let getSchedule = function() {
+    axios({
+        method: "get",
+        url: 'http://api.sportradar.us/ncaafb-t1/2018/REG/schedule.json?api_key=sg8pf7bdjt5u8ueedttyytwx',
+    }).then(data => {
+        let weeks = data.data.weeks;
+        for (let i = 0; i < weeks.length; i++) {
+            for (let j = 0; j < weeks[i].games.length; j++) {
+                for (let k = 0; k < fullTeams.length; k++) {
+                    if (weeks[i].games[j].home == teamAbb[k]) {
+                        pubRoot.post(`/teams/` + fullTeams[k] + `/schedule`, {
+                            "data": weeks[i].games[j].away,
+                            "type": "merge",
+                        })
+                    } else if (weeks[i].games[j].away == teamAbb[k]) {
+                        pubRoot.post(`/teams/` + fullTeams[k] + `/schedule`, {
+                            "data": weeks[i].games[j].home,
+                            "type": "merge",
+                        })
+                    }
+                }
+            }
+        }
+    })
+}
+setTimeout(function() {
+    getSchedule();
+}, 1000);
