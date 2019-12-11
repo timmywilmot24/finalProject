8ce3e2327ff86316def83bad8f30733041d46301
timmywilmot24 <56287362+timmywilmot24@users.noreply.github.com>
2019-12-10 23:13:40 -0500

    Update schedule.js

--- schedule.js
+++ schedule.js
@@ -1,47 +1,12 @@
 export const makeSchedule = function() {
     let sched = [];
-    let w1 = {
-        1: {team1: 1, team2: 2},
-        2: {team1: 3, team2: 4},
-        3: {team1: 5, team2: 6},
-        4: {team1: 7, team2: 8},
-    };
-    let w2 = {
-        1: {team1: 1, team2: 3},
-        2: {team1: 2, team2: 4},
-        3: {team1: 5, team2: 7},
-        4: {team1: 6, team2: 8},
-    };
-    let w3 = {
-        1: {team1: 1, team2: 4},
-        2: {team1: 2, team2: 3},
-        3: {team1: 6, team2: 7},
-        4: {team1: 5, team2: 8},
-    };
-    let w4 = {
-        1: {team1: 1, team2: 5},
-        2: {team1: 2, team2: 7},
-        3: {team1: 3, team2: 6},
-        4: {team1: 4, team2: 8},
-    };
-    let w5 = {
-        1: {team1: 1, team2: 6},
-        2: {team1: 2, team2: 5},
-        3: {team1: 4, team2: 7},
-        4: {team1: 3, team2: 8},
-    };
-    let w6 = {
-        1: {team1: 1, team2: 7},
-        2: {team1: 3, team2: 5},
-        3: {team1: 4, team2: 6},
-        4: {team1: 2, team2: 8},
-    };
-    let w7 = {
-        1: {team1: 1, team2: 8},
-        2: {team1: 2, team2: 6},
-        3: {team1: 3, team2: 7},
-        4: {team1: 4, team2: 5},
-    };
+    let w1 = [[1,2],[3,4],[5,6],[7,8]];
+    let w2 = [[1,3],[2,4],[5,7],[6,8]];
+    let w3 = [[1,4],[2,3],[6,7],[5,8]];
+    let w4 = [[1,5],[2,7],[3,6],[4,8]];
+    let w5 = [[1,6],[2,5],[4,7],[3,8]];
+    let w6 = [[1,7],[3,5],[4,6],[2,8]];
+    let w7 = [[1,8],[2,6],[3,7],[4,5]];
     sched.push(w1, w2, w3, w4, w5, w6, w7, w1, w2, w3, w4, w5);
     axios({
         method: "post",
